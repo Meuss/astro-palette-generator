fd0ed4fe0715ab593903348ad44e26a0e24b8c33
Thomas Miller <thomas.miller147@gmail.com>
2023-10-10 23:54:39 +0200

    🔧

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,16 @@
 module.exports = {
-  extends: [
-    "plugin:astro/recommended",
-    "prettier"
-  ],
+  extends: ['plugin:astro/recommended', 'prettier'],
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
-      rules: {
-      },
+      rules: {},
     },
   ],
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    colors: {
+      black: '#13151a',
+    },
+    extend: {},
+  },
+  plugins: [],
 }
